019d07abb271a6df5d2951ba7ed215519bf55b1f
Alexey Volkov <alexey.volkov@maxoptra.com>
2023-10-21 09:44:26 +0200

    Revert "Done homework"
    This reverts commit 87fe692c39de679cd306e3ca17d5a7c3002e162a.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();
     }
 
     public static void task1(){
@@ -13,12 +12,5 @@ public class Main {
     public static void task2(){
         String a = "Test";
         System.out.println(a);
-    }
-
-    public static void task3(){
-        int b = 1;
-        int a = 2;
-        int c = a+b;
-        System.out.println(c);
     }
 }
