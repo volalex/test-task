9198507e99e15dad7049c67c643a77e96fc074fa
Alexey Volkov <alexey.volkov@maxoptra.com>
2023-10-21 09:23:59 +0200

    Task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1(){
+        int a = 123;
+        System.out.println(a);
     }
 }
